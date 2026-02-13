238b544e221a2006f8f890189b2aa9e3852b3707
Nandana Gireesh <23mp2183@rit.ac.in>
2026-02-13 19:32:47 +0530

    Added employee module files

---
+++ hr_portal/hr_portal_project/employee/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.employee_login, name='employee_login'),
+    path('dashboard/', views.employee_dashboard, name='employee_dashboard'),
+    path('profile/', views.employee_profile, name='employee_profile'),
+    path('leaves/', views.employee_leaves, name='employee_leaves'),
+    
+
+    path('tasks/', views.employee_tasks, name='employee_tasks'),
+    path('attendance/', views.attendance_history, name='attendance_history'),
+    path('announcements/', views.employee_announcements, name='employee_announcements'),
+
+    path('clock-in/', views.clock_in, name='clock_in'),
+    path('clock-out/', views.clock_out, name='clock_out'),
+
+]
