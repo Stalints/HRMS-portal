12e1ca4b2fe195eb7185a7e5e53025e9e13608b4
Stalints <stalintsunni@gmail.com>
2026-02-24 13:20:27 +0530

    Remove hr_portal folder after migration

--- hr_portal/hr_portal_project/employee/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Event,EmployeeProfile, Leave, Task, Attendance, Announcement
-
-admin.site.register(EmployeeProfile)
-admin.site.register(Leave)
-admin.site.register(Task)
-admin.site.register(Attendance)
-admin.site.register(Announcement)
-
